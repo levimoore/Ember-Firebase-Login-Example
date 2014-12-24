3ad38b8ffc0b031092b2300a8021e9de8a2370b3
levimoore <levimoore@gmail.com>
2014-12-24 11:59:35 -0800

    create signup controller

---
+++ app/controllers/signup.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+
+var ref = new Firebase("https://<YOUR FIREBASE INFO>.firebaseio.com");
+
+export default Ember.Controller.extend({
+
+reset: function() {
+    this.setProperties({
+      userEmail: "",
+      userPassword: "",
+      error: ""
+    });
+  },
+
+  token: localStorage.token,
+  tokenChanged: function() {
+    localStorage.token = this.get('token');
+  }.observes('token'),
+
+	actions: {
+	signUp: function(){
+	var _this = this;
+	  ref.changePassword({
+	  email       : this.get('userEmail'),
+	  oldPassword : this.get('oldPassword'),
+	  newPassword : this.get('newPassword')
+	}, function(error, authData) {
+	  if (error === null) {
+	  	ref.authWithPassword({
+	  		email: _this.get('userEmail'),
+	  		password: _this.get('newPassword')
+	  	}, function(error, authData){
+	  		if (error === null) {
+	  			console.log("User logged in.");
+	  			_this.set('token', authData.token);
+	  		} else {
+	  			console.log('User not logged in.');
+	  		}
+	  	});
+	    console.log("Password changed successfully");
+	  } else {
+	    console.log("Error changing password:", error);
+	    _this.set('error', error);
+	  }
+	});
+	}
+}
+});
