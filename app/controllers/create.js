bdc71bbd485864d26824080af2fc51f27323734d
levimoore <levimoore@gmail.com>
2014-12-24 12:13:25 -0800

    created controller for create user

---
+++ app/controllers/create.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+var ref = new Firebase("https://<YOUR FIREBASE INFO>.firebaseio.com");
+
+export default Ember.Controller.extend({
+  authed: false,
+  currentUser: null,
+
+  actions: {
+
+    createUser: function() {
+	    var isNewUser = true;
+	    var _this = this;
+		ref.createUser({
+		  email: this.get('createEmail'),
+		  password: Math.random().toString(36).substring(7)
+	},
+
+	function(error) {
+	  if (error === null) {
+	  	ref.resetPassword({
+	    email : _this.get('createEmail')
+	  }, function(error) {
+	  if (error === null) {
+	    console.log("Password reset email sent successfully");
+	  } else {
+	    console.log("Error sending password reset email:", error);
+	  }
+	});
+		    console.log("User created successfully");
+		  } else {
+		  	_this.set('error', error);
+		    console.log(error);
+		  }
+		  _this.transitionToRoute('email');
+		});
+		}
+	}
+});
