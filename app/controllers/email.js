206581fd7f7411d63eeebd046ee48f9b10ad8c48
levimoore <levimoore@gmail.com>
2014-12-28 15:00:57 -0800

    added email controller for password reset

---
+++ app/controllers/email.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+var ref = new Firebase("https://ember-login-test.firebaseio.com");
+
+export default Ember.Controller.extend({
+
+reset: function() {
+    this.setProperties({
+      userEmail: "",
+      userPassword: "",
+      error: ""
+    });
+  },
+
+  token: localStorage.token,
+  tokenChanged: function() {
+    localStorage.token = this.get('token');
+  }.observes('token'),
+
+	actions: {
+	signUp: function(){
+	var _this = this;
+	  ref.changePassword({
+	  email       : this.get('userEmail'),
+	  oldPassword : this.get('oldPassword'),
+	  newPassword : this.get('newPassword')
+	}, function(error, authData) {
+	  if (error === null) {
+	  	ref.authWithPassword({
+	  		email: _this.get('userEmail'),
+	  		password: _this.get('newPassword')
+	  	}, function(error, authData){
+	  		if (error === null) {
+	  			console.log("User logged in.");
+	  			_this.set('token', authData.token);
+	  		} else {
+	  			console.log('User not logged in.');
+	  		}
+	  	});
+	    console.log("Password changed successfully");
+	  } else {
+	    console.log("Error changing password:", error);
+	    _this.set('error', error);
+	  }
+	   _this.transitionToRoute('secret');
+	});
+	}
+}
+});
